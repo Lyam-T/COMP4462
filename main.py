acc14382a39fa074aa1aef6f342a480838de8c64
daisiyoming <daisyhar11@gmail.com>
2024-11-04 09:25:39 +0800

    Add files via upload
    Framework of the dashboard

---
+++ main.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from dash import Dash, _dash_renderer, Input, Output,html,dcc, callback
+import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
+import plotly.express as px
+
+#read data from csv using pd
+#df = pd.read_csv()
+
+
+#Create the treemap fig
+fig_one = px.treemap(names = ["Eve","Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+    parents = ["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve"])
+fig_one.update_traces(root_color="lightgrey")
+fig_one.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig_two = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig_three = px.pie(names=['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen'], values=[4500, 2500, 1053, 500], title="Atmosphere Composition")
+
+#initialize the app
+app = Dash()
+
+#app layout
+app.layout = dmc.Container([
+    dmc.Title('Movie and TV Shows Data Visualiztaion',color="blue", size="h3"),
+    dmc.Title("COMP4462 Group 8 (Daisy Har, Aatrox Deng, Lyam Tang)", size ="h6" ),
+    dcc.Graph(id="graph-three", figure= fig_three),
+    dmc.Grid([
+        dmc.Col([
+            dcc.Graph(id='graph-one',figure=fig_one)
+        ], span=6),
+        dmc.Col([
+            dcc.Graph(id="graph-two", figure=fig_two)
+        ], span=6)
+    ]),
+    dmc.Grid([
+        dmc.Col([
+            dmc.Text("Dropdown selectors for Tree Map")
+        ], span=6),
+        dmc.Col([
+            dmc.Text("Dropdown selectors for Choropleth Map")
+        ], span=6)
+
+    ]),
+    dmc.Grid([
+        dmc.Col([
+            dcc.Dropdown(['Genres', 'Rating','Directors'], multi=True)
+        ], span=6),
+        dmc.Col([
+            dcc.Dropdown(['Genres', 'Rating','Directors'])
+        ], span=6)
+
+    ])
+
+
+], fluid=True)
+
+#Add interactive buttons
+
+
+#run app
+if __name__ == '__main__':
+    app.run(debug=True)
+
